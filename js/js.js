1ba69a4b92511b5ade902c9d6274057ae7c47309
mina1971 <jommina62@gmail.com>
2021-03-20 17:21:58 +0200

    after replace

---
+++ js/js.js
@@ -0,0 +1,16 @@
+// شيفرة الشرط الخاصة بتشغيل الفيديو
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+    if (myVideo.paused) 
+        myVideo.play();
+    else 
+        myVideo.pause(); 
+}
+// والتي تظهر رسالة خطأ عندما يقوم المستخدم بإدخال معلومات خاطئة validate() شيفرة التابع
+
+$(document).ready(function() {
+    $(function () {
+        $("#commentForm").validate();
+    });
+});
